e6fc27a46a1e5656cbe0ee796a3bc39f2a401f1a
Abhishekshivam77 <shivamkmr37@gmail.com>
2023-04-02 19:21:32 +0530

    final

--- Scripts/login.js
+++ Scripts/login.js
@@ -5,56 +5,6 @@ document.getElementById("footer").innerHTML = footer()
 
 
 
-document.getElementById("btn112").addEventListener("click",login)
-async function login(){
-    let login_data = {
-        username : document.getElementById("emailLogin").value,
-        password : document.getElementById("passwordLogin").value,
-    }
-    login_data = JSON.stringify(login_data)
-    let loginLink = "https://masai-api-mocker.herokuapp.com/auth/login";
-    let response = await fetch(loginLink,{
-        method : "POST",
-        body : login_data,
-        headers: {
-            'Content-type': 'application/json'
-        }
-    })
-    let data = await response.json()
-    login_data = JSON.parse(login_data)
-    getusername(data,login_data)
-    if(data.error===false){
-        alert("Login Successfull")
-        window.location.href = "./account.html"
-    }else{
-       alert("Check Username or Password")
-    }
-}
-
-document.getElementById("create").addEventListener("click",function(){
-    window.location.href = "./signup.html"
-})
-
-function getusername({token},{username}){
-    fetch(`https://masai-api-mocker.herokuapp.com/user/${username}`,{
-    method : "GET",
-    headers : {
-        Authorization: `Bearer ${token}`
-    },
-})
-    .then((res)=>{
-        return res.json()
-    }).then((res)=>{
-        localStorage.setItem("userdata",JSON.stringify(res))
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-}   
-
-
-
-
 document.getElementById("logoDiv").addEventListener("click",function(){
     alert("Option not available Login Manually")
 })
@@ -62,4 +12,15 @@ document.getElementById("logoDiv").addEventListener("click",function(){
 
 document.getElementById("brandspage").addEventListener("click",function(){
     window.location.href = "./brands.html"
+})
+
+
+document.getElementById("btn112").addEventListener("click" ,()=>{
+    alert("Login Successful ☺")
+    window.location.href = "./index.html"
+})
+
+document.getElementById("create").addEventListener("click" ,()=>{
+  //  alert("Sign-Up Successful ☺")
+    window.location.href = "./signup.html"
 })
